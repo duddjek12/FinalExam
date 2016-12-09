da3496f8771d2233b66a4fc17ea44401a2a8db3c
duddjek12 <abra132@naver.com>
2016-12-09 18:56:13 +0900

    커밋 완전 까먹고있었네 ㅜㅜ

--- app/src/main/java/com/example/a403/finalexam/MainActivity.java
+++ app/src/main/java/com/example/a403/finalexam/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a403.finalexam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Finish,timereserve,refinish,Back;
     }
 }
